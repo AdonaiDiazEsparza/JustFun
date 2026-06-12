41380a1d2d16ebf1cbc9955be4e50ca8db53261f
Donuts Diaz <159488665+AdonaiDiazEsparza@users.noreply.github.com>
2026-06-12 09:59:04 -0600

    add MT compilation

--- FilesWithHalosGate/build.bat
+++ FilesWithHalosGate/build.bat
@@ -1,11 +1,10 @@
-
 @echo off
 
 set OUTPUTNAME=program
 
 ml64 /nologo /c /Zi /Cp solve_funcs.asm
 
-cl /nologo /O2 /Zi /utf-8 /EHa /Fe%OUTPUTNAME%.exe EvadeEDR.cpp solve_funcs.obj
+cl /nologo /MT /O2 /Zi /utf-8 /EHa /MANIFEST /MANIFESTFILE:manifest.xml /Fe%OUTPUTNAME%.exe EvadeEDR.cpp solve_funcs.obj
 
 del /s /q "*.obj"
 del /s /q "*.ilk"
